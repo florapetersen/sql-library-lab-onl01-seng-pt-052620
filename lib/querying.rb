4c4d444bcff9062a86183e7ca96116bfae418b5d
Flora Petersen <floramarina@comcast.net>
2020-08-05 00:28:27 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -36,5 +36,9 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name, COUNT(character_books.book_id)
+  FROM characters
+  INNER JOIN characters ON character_books.character_id = characters.id
+  GROUP BY characters.name
+  ORDER BY COUNT(character_books.book_id) DESC;"
 end
