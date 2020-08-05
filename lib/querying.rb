84258c225c07e3851bb1f4210c74b315e0e6b3d8
Flora Petersen <floramarina@comcast.net>
2020-08-05 00:21:25 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -32,7 +32,7 @@ def select_series_title_with_most_human_characters
   INNER JOIN characters
   ON characters.series_id = series.id
   GROUP BY(series.title)
-  ORDER BY(COUNT(characters.species)) HAVING characters.species = 'human'
+  ORDER BY(COUNT(characters.species)) WHERE characters.species = 'human'
   DESC LIMIT 1;"
 end
 
